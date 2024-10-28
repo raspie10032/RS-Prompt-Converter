32e24033abe8dc49277f113254ae9cde090d8cfb
raspie10032 <eseine2@gmail.com>
2024-10-28 14:01:34 +0900

    reference
    reference

--- prompt_converter_gui.py
+++ prompt_converter_gui.py
@@ -1,3 +1,11 @@
+"""
+Original source code credit:
+The prompt_to_stack function is based on the code from:
+https://github.com/bedovyy/ComfyUI_NAIDGenerator/blob/master/utils.py#L146
+Author: bedovyy
+Modified for Novel AI to ComfyUI conversion purposes.
+"""
+
 import tkinter as tk
 from tkinter import ttk
 import re
